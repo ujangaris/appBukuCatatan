b49c8be233b1dd4bf17d72fe5943cfc926f1473f
ujangaja <ujangaja@gmail.com>
2019-12-24 21:24:29 +0700

    Setup view engine ejs

--- helo-express/app.js
+++ helo-express/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = require('./routes/router.js');
 const app = express();
 
+//Setup Views
+app.set('view engine', 'ejs')
+
 
 //Setup Routes
 app.use(router);
