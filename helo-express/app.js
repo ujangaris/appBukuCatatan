595fdccaf905cea67fa88daef4193649b747429e
ujangaja <ujangaja@gmail.com>
2019-12-24 21:32:47 +0700

    Setup files static

--- helo-express/app.js
+++ helo-express/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = require('./routes/router.js');
 const app = express();
 
+//Static File
+app.use(express.static('public'));
+
 //Setup Views
 app.set('view engine', 'ejs')
 
