6b3264144063236c0f35cef37b88675719d3fd42
ujangaja <ujangaja@gmail.com>
2019-12-25 05:19:30 +0700

    Setup layouts

--- helo-express/app.js
+++ helo-express/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const layouts = require('express-ejs-layouts');
 const router = require('./routes/router.js');
 const app = express();
-
 //Static File
 app.use(express.static('public'));
 
 //Setup Views
 app.set('view engine', 'ejs')
+app.use(layouts);
 
 
 //Setup Routes
